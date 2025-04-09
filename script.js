56b413ce1661ad8d372ed0f0edbf18bc1385589b
Archie Goriachev <ufologic@protonmail.com>
2025-04-09 23:55:14 +0300

    Little tweaks. Nothing changed

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 // // init Masonry
-// document.addEventListener("DOMContentLoaded", function () {
-//   const grid = document.querySelector(".grid");
-//   const msnry = new Masonry(grid, {
-//     itemSelector: ".grid-item",
-//     columnWidth: ".grid-sizer",
-//     gutter: 20,
-//     percentPosition: true,
-//     horizontalOrder: false,
-//     transitionDuration: '0.6s',
-//     stagger: 30
-//   });
-
-//   imagesLoaded(grid).on("progress", () => msnry.layout());
-// });
+document.addEventListener("DOMContentLoaded", () => {
+        alert("DOM fully loaded and parsed");
+    });
