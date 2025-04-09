746dd02a61e682889dd3c2ce2ad5b041516669e0
Arsenii Goriachev <ufologic@protonmail.com>
2025-04-09 16:26:41 +0300

    Discard the masonry library. New responsive layout using columns. Added social media links. Centered the page.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// init Masonry
-document.addEventListener("DOMContentLoaded", function () {
-  const grid = document.querySelector(".grid");
-  const msnry = new Masonry(grid, {
-    itemSelector: ".grid-item",
-    columnWidth: ".grid-sizer",
-    gutter: 20,
-    percentPosition: true,
-    horizontalOrder: false,
-    transitionDuration: '0.6s',
-    stagger: 30
-  });
+// // init Masonry
+// document.addEventListener("DOMContentLoaded", function () {
+//   const grid = document.querySelector(".grid");
+//   const msnry = new Masonry(grid, {
+//     itemSelector: ".grid-item",
+//     columnWidth: ".grid-sizer",
+//     gutter: 20,
+//     percentPosition: true,
+//     horizontalOrder: false,
+//     transitionDuration: '0.6s',
+//     stagger: 30
+//   });
 
-  imagesLoaded(grid).on("progress", () => msnry.layout());
-});
+//   imagesLoaded(grid).on("progress", () => msnry.layout());
+// });
